910fa7fe28605440af7bbc79cb6fefbe25bac20a
Daniel <d@loscuenca.com>
2025-07-09 16:45:22 +0200

    debug strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dancuenc <dancuenc@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 17:45:10 by dancuenc          #+#    #+#             */
-/*   Updated: 2025/07/09 16:41:55 by dancuenc         ###   ########.fr       */
+/*   Updated: 2025/07/09 16:44:55 by dancuenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if (!s1 || !s2)
+	if (!s1 || !s2 || n == 0)
 	{
 		return (0);
 	}
-	if (n == 0)
-		return (0);
 	i = 0;
 	while (i < n && (s1[i] || s2[i]))
 	{
